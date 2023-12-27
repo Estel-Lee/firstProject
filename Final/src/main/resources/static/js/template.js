761c40cdfd6246b43fa1a96a543a836b45b5e9ff
kimnahyun <kisuky4@daum.net>
2023-12-27 17:27:59 +0900

    UserInfo(회원가입) 페이지 주소 찾기 추가 유효성검사 추가 -> 아이디, 비밀번호, 이메일

--- Final/src/main/resources/static/js/template.js
+++ Final/src/main/resources/static/js/template.js
@@ -204,5 +204,70 @@
   }
   function getIndex(element) {
     return [...element.parentNode.children].indexOf(element);
-  }
+  }  
 })();
+
+  function checkAll(){
+    if (!checkUserId(form.userid.value)) { 
+      return false; 
+    } else if (!checkPassword(form.userid.value, form.password.value, form.password2.value)) {
+      return false; 
+    } else if (!checkMail(form.useremail.value, form.useremail1.value)) {
+      return false; 
+    } else if (!checkName(form.username.value)) { 
+      return false; 
+    } return true;
+};
+
+function checkUserId(id){
+  var idRegExp = /^[a-zA-z0-9]{4,12}$/;
+  //아이디 유효성 검사        
+  if(!idRegExp.test(id)){
+    alert("아이디는 영문 대소문자와 숫자 4~12자리로 입력해야합니다!");
+    form.userId.value = "";
+    form.userId.focus();
+    return false;
+  }
+  return true;
+}
+
+function checkPassword(id, password, password2){
+    var passwordRegExp = /^[a-zA-z0-9]{4,12}$/;
+    
+    //비밀번호 유효성 검사        
+    if (!passwordRegExp.test(password)) {
+      alert("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해야합니다!");
+      form.password.value = "";
+      form.password1.focus();
+      return false;
+    }
+    //비밀번호와 비밀번호 확인이 맞지 않을 경우    
+    if (password!= password2) {
+     alert("두 비밀번호가 맞지 않습니다.");
+     form.password.value = "";
+     form.password2.value = "";
+     form.password2.focus();
+     return false;
+    }
+    //아이디와 비밀번호가 같을 때        
+    if (id == password) {
+      alert("아이디와 비밀번호는 같을 수 없습니다!");
+      form.password.value = "";
+      form.password2.value = "";
+      form.password2.focus();
+      return false;
+    }return true;
+}
+
+function checkMail(useremail, useremail1) {
+  var emailRegExp = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+  
+  if (!emailRegExp.test(useremail, useremail1)) {
+    alert("이메일 형식이 올바르지 않습니다!");
+    form.useremail.value = "";
+    form.useremail1.value = "";
+    form.useremail.focus();
+    return false;
+  }
+  return true;
+};
