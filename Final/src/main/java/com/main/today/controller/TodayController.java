10df4cd143b1b0fc0b75aa78554fd92db07a722b
kimnahyun <kisuky4@daum.net>
2024-01-05 11:26:20 +0900

    24.01.05
    주석 삭제 및 Board(지역별추천) ->BoardDayarea 이름 변경

--- Final/src/main/java/com/main/today/controller/TodayController.java
+++ Final/src/main/java/com/main/today/controller/TodayController.java
@@ -15,82 +15,64 @@ public class TodayController {
 	@Autowired
 	private Biz biz;
 	
+	//로그인
 	@RequestMapping("/Login")
 	public String Login() {
 		return "Login";
-	}
-
-	@RequestMapping("/Board")
-	public String Board() {
-		return "Board";
-	}
+	}	
 	
+	//문의 게시판
 	@RequestMapping("/CafeBoard")
 	public String CafeBoard() {
 		return "CafeBoard";
 	}
 	
+	//카페 정보
 	@RequestMapping("/PageInfo")
 	public String PageInfo() {
 		return "PageInfo";
 	}
 	
+	//회원 가입
 	@RequestMapping("/UserInfo")
 	public String UserInfo() {
 		return "UserInfo";
 	}
 	
+	//회원 가입 완료
 	@RequestMapping("/UserSuccess")
 	public String UserSuccess() {
 		return "UserSuccess";
 	}
 	
+	//날짜별 추천
 	@RequestMapping("/BoardDaySearch")
 	public String BoardDaySearch() {
 		return "BoardDaySearch";
 	}
 	
+	//테마별 추천
 	@RequestMapping("/BoardDayThema")
 	public String BoardDayThema() {
 		return "BoardDayThema";
 	}
+
+	//지역별 추천
+	@RequestMapping("/BoardDayarea")
+	public String BoardDayarea() {
+		return "BoardDayarea";
+	}
 	
+	
+	//ID 중복체크
 	@ResponseBody
 	@RequestMapping(value = "/checkId", method = RequestMethod.POST)
     public String checkId(TodayMember id) throws Exception{
-		System.out.println("넘어오나요...");
         int result = biz.checkId(id);
-
-		System.out.println("id값 === " + id.getUser_id());
-		
         if(result == 1) {
-        	System.out.println(result + " =------- result값");
         	return "1";
         }
         return "0";
 	} 
-	
-	
-//	@RequestMapping(value="/checkId", method=RequestMethod.POST)
-//	@ResponseBody
-//	public Map<String, Boolean> checkId(@RequestBody TodayMember dto){
-//		
-//		boolean check = false;
-//		Map<String, Boolean> map = new HashMap<>();
-//		
-//		String dto1 = biz.checkId(dto);
-//		
-//		if(dto1 == null) {
-//			check = true;
-//			map.put("check", check);
-//			return map;
-//		}else {
-//			map.put("check", check);
-//			return map;
-//		}
-//	}
-	
-	
-	
-	
+
 }
